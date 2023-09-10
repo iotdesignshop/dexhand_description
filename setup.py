4aa9fdf48cd28ea67052f2a82f1b55d1aac1cb97
Trent Shumay <trent@iotdesignshop.com>
2023-09-10 07:35:20 -0700

    Baseline launch file and RVIZ config

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
+from setuptools import setup
+from setuptools import find_packages
 
 package_name = 'dexhand_description'
 
@@ -10,6 +14,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
