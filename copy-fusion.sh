169ec1739c99145e14f2bdea0a53ce2d8a7a85fe
Trent Shumay <trent@iotdesignshop.com>
2023-09-10 20:56:13 -0700

    Working finger set

--- copy-fusion.sh
+++ copy-fusion.sh
@@ -1,5 +1,5 @@
 rm ./urdf/*
-cp /media/psf/Home/Temp/dexhand-urdf/dexhand_description/urdf/* ./urdf
+cp /media/psf/Home/Temp/dexhand_description/urdf/* ./urdf
 rm ./meshes/*
-cp /media/psf/Home/Temp/dexhand-urdf/dexhand_description/meshes/* ./meshes
+cp /media/psf/Home/Temp/dexhand_description/meshes/* ./meshes
 
